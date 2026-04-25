95f4a04917482bd0b20ec57290062d6202d5cc15
ocelot910 <ocelot910@proton.me>
2026-04-25 21:10:53 +1000

    Add linux support

--- TIPatcher/Patcher.cs
+++ TIPatcher/Patcher.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO.Compression;
+using System.Runtime.InteropServices;
 using System.Text;
 using TIPatcher.Interfaces;
 
@@ -19,11 +20,28 @@ namespace TIPatcher
     {
         // constants
         private static string _tiUrl => """https://education.ti.com/en/software/details/en/36BE84F974E940C78502AA47492887AB/ti-nspirecxcas_pc_full""";
-        private static string _tiPath => """C:\Program Files\TI Education\TI-Nspire CX CAS Student Software\""";
-        private static string _libPath => Path.Combine(_tiPath + "lib");
+        private static string _tiPath
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    return Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                        ".wine",
+                        "drive_c",
+                        "Program Files",
+                        "TI Education",
+                        "TI-Nspire CX CAS Student Software");
+                }
+                // assume Windows
+                return """C:\Program Files\TI Education\TI-Nspire CX CAS Student Software\""";
+            }
+        }
+        private static string _libPath => Path.Combine(_tiPath, "lib");
         private static string _tiName => "TI-Nspire CX CAS Student Software";
         private static string _tiExeName => _tiName + ".exe";
-        private static string _classPath => """com\ti\et\phoenix\jni\ApplWrapper.class""";
+        private static string _classPath => Path.Combine("com", "ti", "et", "phoenix", "jni", "ApplWrapper.class");
         private static string _jarName => "docfw.jar";
         private static string _tempDirname => "Temp";
         private static string[] _fieldsToPatch => ["isLicenseCheckEnabled", "isVerificationEnabled"];
@@ -37,9 +55,16 @@ namespace TIPatcher
         public bool Patch()
         {
             logger.Log("TIPatcher: Starting patch...");
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                logger.Log("TIPatcher: TIPatcher doesn't support MacOS! Quitting...");
+                return false;
+            }
+
             if (!File.Exists(PathToJar))
             {
-                logger.Log("TIPatcher: Can't find docfw jar! You need to install " + _tiName);
+                logger.Log($"TIPatcher: Can't find docfw jar at {PathToJar}! You need to install " + _tiName);
 
                 logger.Log("TIPatcher: Do you want to open the download page for " + _tiName + "? (y/n)");
                 string response = logger.Ask();
@@ -66,6 +91,7 @@ namespace TIPatcher
             }
 
             TempDir = Path.Combine(OutputDir, _tempDirname);
+            logger.Log($"TIPatcher: Using temporary directory {TempDir}");
 
             if (Directory.Exists(TempDir))
             {
@@ -147,20 +173,24 @@ namespace TIPatcher
             if (Path.Exists(_libPath))
             {
                 logger.Log($"TIPatcher: Do you want to copy the patched file automatically to {_libPath}?");
-                logger.Log($"TIPatcher: This will require administrator privileges.");
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    logger.Log($"TIPatcher: This will require administrator privileges.");
+                }
                 logger.Log($"TIPatcher: Copy? (y/n)");
                 string response = logger.Ask();
 
                 if (response.Contains('y'))
                 {
                     Process[] processes = Process.GetProcessesByName(_tiName);
-                    if (processes.Length == 1)
+                    foreach (Process process in processes)
                     {
                         logger.Log("TIPatcher: " + _tiName + " is running! Stopping " + _tiExeName);
 
                         try
                         {
-                            processes[0].Kill();
+                            process.Kill();
+                            process.WaitForExit();
                             logger.Log("TIPatcher: Successfully killed " + _tiExeName);
                         }
                         catch
@@ -170,21 +200,19 @@ namespace TIPatcher
                     }
 
 
-                    ProcessStartInfo psi = new()
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                     {
-                        FileName = "cmd.exe",
-                        Arguments = $"/c copy /Y \"{Path.Combine(OutputDir, _jarName)}\" \"{_libPath}\"",
-                        Verb = "runas",
-                        UseShellExecute = true
-                    };
+                        logger.Log("TIPatcher: Linux detected!");
 
-                    try
-                    {
-                        Process? p = Process.Start(psi);
-                        p?.WaitForExit();
-                        if (p?.ExitCode == 0)
+                        try
                         {
-                            logger.Log("TIPatcher: Copying successful!");
+                            File.Copy(
+                                Path.Combine(OutputDir, _jarName),
+                                Path.Combine(_libPath, _jarName),
+                                overwrite: true
+                            );
+
+                            logger.Log("TIPatcher: Patched successfully!");
 
                             logger.Log("TIPatcher: Do you want to launch " + _tiName + "? (y/n)");
 
@@ -196,7 +224,11 @@ namespace TIPatcher
                                 {
                                     logger.Log("TIPatcher: Starting " + _tiExeName);
 
-                                    ProcessStartInfo tiPSI = new(Path.Combine(_tiPath, _tiExeName));
+                                    ProcessStartInfo tiPSI = new("wine")
+                                    {
+                                        Arguments = $"\"{Path.Combine(_tiPath, _tiExeName)}\""
+                                    };
+
                                     try
                                     {
                                         Process? tiProcess = Process.Start(tiPSI);
@@ -204,19 +236,75 @@ namespace TIPatcher
                                     catch
                                     {
                                         logger.Log("TIPatcher: Failed to start " + _tiExeName + "!");
-
+                                        logger.Log("TIPatcher: This may be due to your Wine configuration");
+                                        return false;
                                     }
                                 }
                             }
                         }
-                        else
+                        catch (DirectoryNotFoundException)
                         {
-                            logger.Log($"TIPatcher: Copy failed. Exit code is {p?.ExitCode}");
+                            logger.Log("TIPatcher: Directory not found (Wine)");
+                            return false;
+                        }
+                        catch
+                        {
+                            logger.Log("TIPatcher: An error occurred while copying the file.");
+                            return false;
                         }
                     }
-                    catch (Win32Exception)
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                     {
-                        logger.Log("TIPatcher: UAC is required to copy to the TI directory, as it is write-protected!");
+                        ProcessStartInfo psi = new()
+                        {
+                            FileName = "cmd.exe",
+                            Arguments = $"/c copy /Y \"{Path.Combine(OutputDir, _jarName)}\" \"{_libPath}\"",
+                            Verb = "runas",
+                            UseShellExecute = true
+                        };
+
+                        try
+                        {
+                            Process? p = Process.Start(psi);
+                            p?.WaitForExit();
+                            if (p?.ExitCode == 0)
+                            {
+                                logger.Log("TIPatcher: Copying successful!");
+
+                                logger.Log("TIPatcher: Do you want to launch " + _tiName + "? (y/n)");
+
+                                string startTi = logger.Ask();
+
+                                if (startTi.Contains('y'))
+                                {
+                                    if (File.Exists(Path.Combine(_tiPath, _tiExeName)))
+                                    {
+                                        logger.Log("TIPatcher: Starting " + _tiExeName);
+
+                                        ProcessStartInfo tiPSI = new(Path.Combine(_tiPath, _tiExeName));
+                                        try
+                                        {
+                                            Process? tiProcess = Process.Start(tiPSI);
+                                        }
+                                        catch
+                                        {
+                                            logger.Log("TIPatcher: Failed to start " + _tiExeName + "!");
+                                            return false;
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                logger.Log($"TIPatcher: Copy failed. Exit code is {p?.ExitCode}");
+                                return false;
+                            }
+                        }
+                        catch (Win32Exception)
+                        {
+                            logger.Log("TIPatcher: UAC is required to copy to the TI directory, as it is write-protected!");
+                            return false;
+                        }
                     }
                 }
                 else
